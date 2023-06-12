33ed8964d6ccbde9c6f7cc8d65d5d6abdec77ed4
AnthonyJegede <anthonyojegede@gmail.com>
2023-06-12 17:22:06 +0100

    Applied frames to two  home images

---
+++ module6/js/script.js
@@ -0,0 +1,19 @@
+
+function sayHello() {
+alert("Hello!")
+}
+
+function getFormvalue()
+{
+	var x=document.getElementById("form1");
+	for (var i=0; i<x.length; i++)
+	{
+		if (x.elements[i].value!='Submit')
+		{
+			console.log(x.elements[i].value);
+			alert(x.elements[i].value);
+		}
+
+
+	}
+}
