a73182cfb218fae020f62cc69f0511e11135fe8a
AnthonyJegede <anthonyojegede@gmail.com>
2023-06-12 20:39:15 +0100

    Increased frame size to avoid scroll bars

--- module6/js/script.js
+++ module6/js/script.js
@@ -17,3 +17,18 @@ function getFormvalue()
 
 	}
 }
+
+function multiplyBy() 
+{
+        num1=document.getElementById("firstNumber").value;
+        num2=document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML=num1*num2;
+}
+
+function divideBy() 
+{ 
+        num1=document.getElementById("firstNumber").value;
+        num2=document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML=num1/num2;
+}
+
